1dee64c0509485397eea12a0c60ac3483295919a
Charith Lakshan <it23841086@my.sliit.lk>
2025-10-01 09:24:43 +0530

    Add fragments and splash activity, update manifest
    Introduced HabitsFragment, HomeFragment, MoodFragment, ProfileFragment, and SplashActivity with corresponding layout files. Updated AndroidManifest.xml to register new activities and set SplashActivity as the launcher. Refactored MainActivity to remove splash logic and simplified its layout.

--- app/src/main/java/com/example/dailyboost/SplashActivity.kt
+++ app/src/main/java/com/example/dailyboost/SplashActivity.kt
@@ -1,20 +1,31 @@
 package com.example.dailyboost
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_activity)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        lifecycleScope.launch {
+            delay(3000) // 3 seconds
+            startActivity(Intent(this@SplashActivity, OnboardScreen1::class.java))
+            finish()
         }
     }
 }
